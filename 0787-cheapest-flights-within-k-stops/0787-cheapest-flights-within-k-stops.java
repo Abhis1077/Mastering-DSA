71a0ccec731249fa194f3d9903edc67dc1719d3a
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-06-21 03:11:06 +0530

    Time: 8 ms (30.05%), Space: 46.7 MB (39.69%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        for(int[] flight : flights){
+            adj.computeIfAbsent(flight[0], z-> new ArrayList<>()).add(new int[]{flight[1],flight[2]});
+        }
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{src,0});
+
+        int stops = 0;
+
+        while(!q.isEmpty() && stops <= k){
+            int size = q.size();
+
+            for(int i = 0; i < size;i++){
+                int[] curr = q.poll();
+
+                int u = curr[0];
+                int wt  = curr[1];
+
+                if(!adj.containsKey(u)){
+                    continue;
+                }
+                for(int[] neigh : adj.get(u)){
+                    int v = neigh[0];
+                    int cost = neigh[1];
+                    if(cost + wt < dist[v]){
+                        dist[v] = cost + wt;
+                        q.offer(new int[]{v, dist[v]});
+                    }
+                }
+
+            }
+            stops++;
+        }
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+
+    }
+}

b0da01a378644a16cc419968bbc8702175546ad9
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-07-05 01:16:23 +0530

    Time: 0 ms (100%), Space: 42 MB (69.6%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int fib(int n) {
 
-//Space optimisation
-        if(n<=1){
+    public int fibon(int n , int[] dp){
+        if(n <=1){
             return n;
         }
-        //int[] dp = new int[n+1];
-
-        int a = 0;
-        int b = 1;
-        int c= 0;
-
-        for(int i = 2 ; i<=n;i++ ){
-            c = a + b;
-            a = b;
-            b = c;
-            
+        if(dp[n] != -1){
+            return dp[n];
         }
+        return dp[n] = fibon(n-1, dp) + fibon(n-2,dp);
+    }
+    public int fib(int n) {
 
-        return c;
+        int[] dp = new int[n+1];
+
+        Arrays.fill(dp,-1);
+        
+        return fibon(n,dp);
     }
 }

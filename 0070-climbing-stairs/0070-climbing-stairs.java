7de5e1509fdb45d8802305f70bbb9952e7c95fca
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-07-05 20:50:30 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,54 @@
 class Solution {
 
-     public int countstairs(int i , int n, int[] dp){
-        if(i==n){
-                return 1;
-            }
-        if(i>n){
-                return 0;
-            }
-        if(dp[i] != -1){
-            return dp[i];
-        }
+    //Top down Approach
+    //  public int countstairs(int i , int n, int[] dp){
+    //     if(i==n){
+    //             return 1;
+    //         }
+    //     if(i>n){
+    //             return 0;
+    //         }
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
 
-        return dp[i] = countstairs(i+1,n,dp) + countstairs(i+2,n,dp);
-    }
+    //     return dp[i] = countstairs(i+1,n,dp) + countstairs(i+2,n,dp);
+    // }
+    // public int climbStairs(int n) {
+    //     int[] dp = new int[n+2];
+    //     Arrays.fill(dp,-1);
+    //     return countstairs(0 ,n,dp);
+    // }
+
+    //Bottom up approach
+    
+    // public int climbStairs(int n) {
+    //     int[] dp = new int[n+2];
+      
+    //      dp[n] = 1;
+            
+     
+    //      dp[n+1] = 0;
+            
+    //     for(int i = n-1 ; i>=0;i--){
+    //          dp[i] = dp[i+1] + dp[i+2];
+    //     }
+    //     return dp[0];
+    // }
+
+    //SPace optimisation
+
     public int climbStairs(int n) {
-        int[] dp = new int[n+2];
-        Arrays.fill(dp,-1);
-        return countstairs(0 ,n,dp);
+        int curr = 0;
+        int prev = 1;
+        int prev1 = 0;
+      
+            
+        for(int i = n-1 ; i>=0;i--){
+            curr = prev + prev1;
+            prev1 = prev;
+            prev = curr;
+        }
+        return curr;
     }
 }

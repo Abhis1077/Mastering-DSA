a69d256c1b972e35a34bd104ff3a904bd69a6828
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-07-13 02:36:05 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,27 +1,69 @@
 class Solution {
 
-    public int countMoney(int i , int[] nums, int[] dp){
+    // public int countMoney(int i , int[] nums, int[] dp){
         
-        if(i == 0){
-            return nums[0];
-        }
-        if(i < 0 ){
-            return 0;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
+    //     if(i == 0){
+    //         return nums[0];
+    //     }
+    //     if(i < 0 ){
+    //         return 0;
+    //     }
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
 
         
-        return dp[i] = Math.max((nums[i] + countMoney(i-2,nums,dp)),countMoney(i-1,nums,dp));
-    }
+    //     return dp[i] = Math.max((nums[i] + countMoney(i-2,nums,dp)),countMoney(i-1,nums,dp));
+    // }
+    // public int rob(int[] nums) {
+     
+    //     int n = nums.length;
+
+    //     int[] dp = new int[n];
+    //     Arrays.fill(dp,-1);
+
+    //     return countMoney(n-1, nums,dp);
+    // }
+
+
+    //Bottom Up
+    // public int rob(int[] nums) {
+     
+    //     int n = nums.length;
+
+    //     int[] dp = new int[n];
+
+    //     dp[0] = nums[0];
+    //     dp[1] = Math.max(nums[0],nums[1]);
+
+    //     for(int i = 2 ; i <n ;i++){
+
+    //         dp[i] = Math.max((nums[i]+dp[i-2]), dp[i-1]);
+    //     }
+    //     return dp[n-1];
+    // }
+
+    //Space Optimisation
+
     public int rob(int[] nums) {
      
         int n = nums.length;
 
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
+        if(n == 0) return 0;
+        if (n==1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
 
-        return countMoney(n-1, nums,dp);
+        int prev1 = nums[0];
+        int prev = Math.max(nums[0],nums[1]);
+        int curr = 0;
+
+        for(int i = 2 ; i <n ;i++){
+
+            curr = Math.max((nums[i]+prev1), prev);
+            prev1 = prev;
+            prev = curr;
+        }
+        return curr;
     }
+
 }

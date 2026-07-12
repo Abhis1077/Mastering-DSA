9fd4ec834213910e3c13bd04addd047c6346b30d
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-07-13 01:37:45 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.85%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int countMoney(int i , int[] nums, int[] dp){
+        
+        if(i == 0){
+            return nums[0];
+        }
+        if(i < 0 ){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        
+        return dp[i] = Math.max((nums[i] + countMoney(i-2,nums,dp)),countMoney(i-1,nums,dp));
+    }
+    public int rob(int[] nums) {
+     
+        int n = nums.length;
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return countMoney(n-1, nums,dp);
+    }
+}

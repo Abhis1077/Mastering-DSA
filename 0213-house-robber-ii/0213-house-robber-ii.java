92cf95005f723bf2dce9c5dba0e528b55137062f
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-08-29 02:02:50 +0530

    Time: 0 ms (100%), Space: 42.6 MB (78.11%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public int countMoney(int i , int[] nums, int[] dp){
+        if(i>=nums.length){
+            return 0;
+        }
+
+        if(dp[i]!= -1){
+            return dp[i];
+        }
+
+        return dp[i] = Math.max((nums[i]+ countMoney(i+2,nums,dp)),countMoney(i+1,nums,dp));
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        int[] dp1 = new int[n-1];
+        int[] dp2 = new int[n-1];
+
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+
+        if(n == 1){
+            return nums[0];
+        }
+
+        int[] arr1 = new int[n-1];
+        int[] arr2 = new int[n-1];
+
+        for( int i = 0 ; i < n-1 ; i++)
+        {
+            arr1[i] = nums[i];
+        }
+        for( int i = 1 ; i < n ; i++)
+        {
+            arr2[i-1] = nums[i];
+        }
+
+        return Math.max(countMoney(0,arr1,dp1),countMoney(0,arr2,dp2));
+    }
+}

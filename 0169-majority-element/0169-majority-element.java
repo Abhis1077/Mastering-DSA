e74a2e79322880ad6110b45e5fa66da3b4e47ba3
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-09-05 20:40:56 +0530

    Time: 2 ms (70.57%), Space: 55.6 MB (46.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,30 +1,48 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
+//         int n = nums.length;
 
-        int majEle = 0;
+//         int majEle = 0;
 
-        Arrays.sort(nums);
-        int current = nums[0];
+//         Arrays.sort(nums);
+//         int current = nums[0];
 
-        int c = 1;
-        for(int i = 1 ; i<n ; i++)
-        {
-            if(current == nums[i]){
-                c++;
-            }
-            else
-            {
-                if(c > n/2){
-                    return current;
-                }
-                current = nums[i];
-                c = 1;
-            }
-        }
-                if(c > n/2){
-                    majEle =  current;
-                }
- return majEle;
+//         int c = 1;
+//         for(int i = 1 ; i<n ; i++)
+//         {
+//             if(current == nums[i]){
+//                 c++;
+//             }
+//             else
+//             {
+//                 if(c > n/2){
+//                     return current;
+//                 }
+//                 current = nums[i];
+//                 c = 1;
+//             }
+//         }
+//                 if(c > n/2){
+//                     majEle =  current;
+//                 }
+//  return majEle;
+
+//OPTIMAL METHOD
+
+int current = 0;
+int count = 0;
+
+for(int num : nums){
+    if(count == 0){
+        current = num;
+    }
+    if(num == current){
+        count++;
+    }
+    else{
+        count--;
+    }
+}
+return current;
     }
 }

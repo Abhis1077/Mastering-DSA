71caed90885eba8ec9cb6da4f3e0dddd58b67cf2
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-09-14 16:59:08 +0530

    Time: 25 ms (90.36%), Space: 100.9 MB (7.05%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,42 +1,94 @@
-class RandomizedSet {
-        Map<Integer,Integer> map = new HashMap<>();
+// class RandomizedSet {
+//         Map<Integer,Integer> map = new HashMap<>();
 
-        List<Integer> list = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
 
-        Random rand = new Random();
-    public RandomizedSet() {
+//         Random rand = new Random();
+//     public RandomizedSet() {
         
+//     }
+    
+//     public boolean insert(int val) {
+//         if(map.containsKey(val)){
+//             return false;
+//         }
+//         map.put(val,list.size());
+//         list.add(val);
+
+//         return true;
+        
+//     }
+    
+//     public boolean remove(int val) {
+//         if(!map.containsKey(val)){
+//             return false;
+//         } 
+//         int targetIndex = map.get(val);
+//         int lastElement = list.get(list.size()-1);
+//         list.set(targetIndex,lastElement);
+//         map.put(lastElement,targetIndex);
+//         list.remove(list.size()-1);
+//         map.remove(val);
+
+//         return true;
+
+//     }
+    
+//     public int getRandom() {
+//         int randomIndex = rand.nextInt(list.size());
+
+//         return list.get(randomIndex);
+//     }
+// }
+
+import java.util.*;
+
+class RandomizedSet {
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+    private Random rand;
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
     }
     
     public boolean insert(int val) {
-        if(map.containsKey(val)){
+        if (map.containsKey(val)) {
             return false;
         }
-        map.put(val,list.size());
+        // Map the value to its index at the end of the array
+        map.put(val, list.size());
         list.add(val);
-
         return true;
-        
     }
     
     public boolean remove(int val) {
-        if(!map.containsKey(val)){
+        if (!map.containsKey(val)) {
             return false;
-        } 
+        }
+        
+        // 1. Get the index of the value to remove
         int targetIndex = map.get(val);
-        int lastElement = list.get(list.size()-1);
-        list.set(targetIndex,lastElement);
-        map.put(lastElement,targetIndex);
-        list.remove(list.size()-1);
+        
+        // 2. Grab the last element in the list
+        int lastElement = list.get(list.size() - 1);
+        
+        // 3. Move the last element into the target index position
+        list.set(targetIndex, lastElement);
+        map.put(lastElement, targetIndex);
+        
+        // 4. Remove the last element from list and the target value from map
+        list.remove(list.size() - 1);
         map.remove(val);
-
+        
         return true;
-
     }
     
     public int getRandom() {
+        // Pick a random index from 0 to list.size() - 1
         int randomIndex = rand.nextInt(list.size());
-
         return list.get(randomIndex);
     }
 }

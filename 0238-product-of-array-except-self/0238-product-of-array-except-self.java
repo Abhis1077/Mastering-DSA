ed46474b442b864ebba47ff416f6917560838f63
Abhishek Raj <87387647+Abhis1077@users.noreply.github.com>
2026-09-19 03:38:20 +0530

    Time: 2 ms (92.77%), Space: 72 MB (43.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,17 +3,17 @@ class Solution {
         int n = nums.length;
 
         int[] answer = new int[n];
-        int leftP = 1;
         int rightP = 1;
         answer[0] = 1;
         for(int i = 1;i<n;i++){
-            answer[i] = nums[i-1]*leftP;
-            leftP = answer[i];
+            answer[i] = nums[i-1]*answer[i-1];
+
         }
 
-        for(int i = n-2;i>=0;i--){
-            rightP = nums[i+1]*rightP;
+        for(int i = n-1;i>=0;i--){
             answer[i] = answer[i]*rightP;
+            rightP = nums[i]*rightP;
+            
         }
 
         return answer;
